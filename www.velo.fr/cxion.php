a777ad6bc912002a9a9dc22dc0351447bb7d223d
Stevann PICHON <s.pichon@workstation6.nsi.lan>
2022-01-25 15:54:51 +0100

    changement

--- www.velo.fr/cxion.php
+++
@@ -1,6 +0,0 @@
-<h3> Mon compte </h3>
-Ton identifiant est : <?php echo htmlspecialchars($_POST['login']);?>.
-<br>
-Ton mot de passe est : <?php echo htmlspecialchars($_POST['password']);?>.
-
-<p> <input type= "submit" value="Aller sur vos données"></p>
